7b32c7d9ee2ed5bfdfec0b60c783f32fbf33ab2d
EdgarP43 <e.paredes-cas@outlook.com>
2020-08-16 15:04:19 -0600

    Pruebas iniciales

---
+++ ProyectoCompiladores2020/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProyectoCompiladores2020
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<int, string> lineas = new Dictionary<int, string>();
+        public List<string> reservadas = new List<string>();
+        analizador palala = new analizador();
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader sr = new StreamReader("Reservadas.txt"))
+            {
+                string linea = "";
+                while ((linea = sr.ReadLine()) != null)
+                {
+                        reservadas.Add(linea);
+                }
+            }
+            palala.guardarReservadas(reservadas);
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int contlineas = 0;
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "Text files(*.txt)|*.txt|Todos los archivos(*.*)|*.*";
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(o.FileName)) 
+                {
+                    string linea = "";
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        contlineas++;
+                        lineas.Add(contlineas,linea);
+                    }
+                }
+            }
+            palala.guardarArchivo(lineas);
+            var mostrar = palala.Reconocedor();
+            foreach (var item in mostrar)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ ProyectoCompiladores2020/analizador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoCompiladores2020
+{
+    class analizador
+    {
+        private List<string> reservadas = new List<string>();
+        private Dictionary<int, string> archivo = new Dictionary<int, string>();
+        public void guardarReservadas(List<string> palabras)
+        {
+            reservadas = palabras;
+        }
+        public void guardarArchivo(Dictionary<int, string> lineas)
+        {
+            archivo = lineas ;
+        }
+
+        public List<string> Reconocedor()
+        {
+            int llave = 1;
+            string concatpalabra = String.Empty;
+            var prueba = new List<string>();
+            do
+            {
+                for(int i = 0; i<reservadas.Count; i++)
+                { 
+                    if (archivo[llave].Contains(reservadas[i])/*lineas.ContainsValue(archivo.)*/)
+                    {
+                        prueba.Add("reservada : "+reservadas[i] + " en linea " + llave.ToString());
+                        
+                    }
+                    if (archivo[i]!="\t" && archivo[i] != "\n" && archivo[i] != " ")
+                    {
+                        concatpalabra += archivo[i];
+                    }
+                }
+                llave++;
+            } while (archivo.ContainsKey(llave));
+            return prueba;
+        }
+    }
+}
